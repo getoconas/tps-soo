3b9fd179b56e5fd6bba15481a93a3d4e3a40ee03
getoconas <getoconas@gmail.com>
2024-09-02 23:33:11 -0300

    tp1 - modificacion de la clase profesional y administrativo

--- TP1_SOO/src/gestionsueldos/dominio/Administrativo.java
+++ TP1_SOO/src/gestionsueldos/dominio/Administrativo.java
@@ -3,6 +3,63 @@
  */
 package gestionsueldos.dominio;
 
-public class Administrativo {
+import java.util.List;
 
+public class Administrativo extends Empleado {
+	private int categoria;
+	
+	/**
+	 * Constructor de la clase Administrativo
+	 * @param id
+	 * @param legajo
+	 * @param nombre
+	 * @param cantidad_hijos
+	 * @param antiguedad
+	 */
+	public Administrativo(int id, int legajo, String nombre, int cantidad_hijos, int antiguedad, int categoria) {
+		super(id, legajo, nombre, cantidad_hijos, antiguedad);
+		this.categoria = categoria;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	/*@Override
+	public Double obtenerSalario() {
+		return 0.0;
+	}*/
+	
+	/**
+	 * 
+	 * @return
+	 */
+	@Override
+	public String toString() {
+		return "";
+	}
+	
+	/**
+	 * 
+	 * @param anio
+	 * @return
+	 */
+	@Override
+	public List mostrarEmpleadoAntiguedad(int anio) {
+		return null;	
+	}
+	
+	/**
+	 * @return the categoria
+	 */
+	public int getCategoria() {
+		return categoria;
+	}
+
+	/**
+	 * @param categoria the categoria to set
+	 */
+	public void setCategoria(int categoria) {
+		this.categoria = categoria;
+	}
 }
--- TP1_SOO/src/gestionsueldos/dominio/Profesional.java
+++ TP1_SOO/src/gestionsueldos/dominio/Profesional.java
@@ -3,6 +3,96 @@
  */
 package gestionsueldos.dominio;
 
-public class Profesional {
+import java.util.List;
+import gestionsueldos.util.Constantes;
 
+public class Profesional extends Empleado {
+	private boolean titulo;
+	private double sueldo_neto;
+	
+	/**
+	 * Constructor de la clase Profesional
+	 * @param id
+	 * @param legajo
+	 * @param nombre
+	 * @param cantidad_hijos
+	 * @param antiguedad
+	 * @param titulo
+	 */
+	public Profesional(int id, int legajo, String nombre, int cantidad_hijos, int antiguedad, boolean titulo) {
+		super(id, legajo, nombre, cantidad_hijos, antiguedad);
+		this.titulo = titulo;
+	}
+	
+	/**
+	 * 
+	 * @param cantidad_hijos
+	 * @param antiguedad
+	 * @param titulo
+	 */
+	public void obtenerSalario(int cantidad_hijos, int antiguedad, boolean titulo) {
+		if (cantidad_hijos > 0) {
+			this.sueldo_neto = this.obtenerRemunerativos(antiguedad, titulo) + this.obtenerPagoPorHijoACargo(cantidad_hijos) - this.obtenerDescuentos(antiguedad, titulo);
+		} else {
+			this.sueldo_neto = this.obtenerRemunerativos(antiguedad, titulo) - this.obtenerDescuentos(antiguedad, titulo);
+		}		
+	}
+	
+	/**
+	 * 
+	 * @param antiguedad
+	 * @param titulo
+	 * @return
+	 */
+	private double obtenerRemunerativos(int antiguedad, boolean titulo) {
+		if (titulo) {
+			return Constantes.SUELDO_BASICO + Constantes.TITULO + this.obtenerPagoPorAntiguedad(antiguedad) ;
+		} else {
+			return Constantes.SUELDO_BASICO + this.obtenerPagoPorAntiguedad(antiguedad) ;
+		}
+	}
+	
+	
+	/**
+	 * 
+	 * @param antiguedad
+	 * @param titulo
+	 * @return
+	 */
+	private double obtenerDescuentos(int antiguedad, boolean titulo) {
+		return this.obtenerRemunerativos(antiguedad, titulo) * Constantes.DESCUENTO;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	@Override
+	public String toString() {
+		return "";
+	}
+	
+	/**
+	 * 
+	 * @param anio
+	 * @return
+	 */
+	@Override
+	public List mostrarEmpleadoAntiguedad(int anio) {
+		return null;	
+	}
+	
+	/**
+	 * @return the titulo
+	 */
+	public boolean isTitulo() {
+		return titulo;
+	}
+
+	/**
+	 * @param titulo the titulo to set
+	 */
+	public void setTitulo(boolean titulo) {
+		this.titulo = titulo;
+	}	
 }
