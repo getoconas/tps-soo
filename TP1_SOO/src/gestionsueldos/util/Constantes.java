d79acf2726be317c6ddf207c483168326900f08b
getoconas <getoconas@gmail.com>
2024-09-02 23:31:56 -0300

    tp1 - creacion constantes

---
+++ TP1_SOO/src/gestionsueldos/util/Constantes.java
@@ -0,0 +1,12 @@
+/**
+ * @author mago
+ */
+package gestionsueldos.util;
+
+public class Constantes {
+	public static final double SUELDO_BASICO = 100000.00;
+	public static final double TITULO = 40000.00;
+	public static final double ANTIGUEDAD = 4000.00;
+	public static final double HIJO_A_CARGO = 3000.00;
+	public static final double DESCUENTO = 0.15;
+}
