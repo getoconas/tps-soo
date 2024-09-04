5f098f532b20e90a224bec2dfd9a46691b63e543
getoconas <getoconas@gmail.com>
2024-09-04 13:33:28 -0300

    tp1 - agregado clase de constantes importes

--- TP1_SOO/src/gestionsueldos/util/Importes.java
+++ TP1_SOO/src/gestionsueldos/util/Importes.java
@@ -1,12 +1,17 @@
 /**
- * @author mago
+ * @author Gaston Toconas
  */
 package gestionsueldos.util;
 
-public class Constantes {
+public class Importes {
 	public static final double SUELDO_BASICO = 100000.00;
 	public static final double TITULO = 40000.00;
 	public static final double ANTIGUEDAD = 4000.00;
 	public static final double HIJO_A_CARGO = 3000.00;
 	public static final double DESCUENTO = 0.15;
+	public static final double CATEGORIA_1 = 5000.00;
+	public static final double CATEGORIA_2 = 7000.00;
+	public static final double CATEGORIA_3 = 9000.00;
+	public static final double CATEGORIA_4 = 15000.00;
+	public static final double CATEGORIA_5 = 20000.00;
 }
