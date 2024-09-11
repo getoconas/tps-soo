e6094924104825dab0372c70ef9f759e2de32cb5
getoconas <getoconas@gmail.com>
2024-09-11 15:33:45 -0300

    tp2 - change test

--- tp2/gestionsueldos/src/test/java/ar/edu/unju/fi/gestionsueldos/AdministrativeTest.java
+++ tp2/gestionsueldos/src/test/java/ar/edu/unju/fi/gestionsueldos/AdministrativeTest.java
@@ -4,52 +4,101 @@ import ar.edu.unju.fi.gestionsueldos.models.Administrative;
 import ar.edu.unju.fi.gestionsueldos.util.Amount;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 public class AdministrativeTest {
-    private Administrative administrative;
+    private Administrative a1, a2, a3, a4, a5;
 
     @BeforeEach
     public void setUp() {
         System.out.println("Configurando SetUp...");
-        administrative = new Administrative();
-        administrative.setId(2);
-        administrative.setRecord(200);
-        administrative.setName("MARIA GOMEZ");
-        administrative.setChildren(2);
-        administrative.setSeniority(3);
-        administrative.setCategory(2);
+        a1 = new Administrative();
+        a2 = new Administrative();
+        a3 = new Administrative();
+        a4 = new Administrative();
+        a5 = new Administrative();
+        newAdministrative(a1, 1, 100, "MARIA GOMEZ", 0, 0, 1);
+        newAdministrative(a2, 2, 110, "MARTA MERCADO", 0, 0, 2);
+        newAdministrative(a3, 3, 120, "LUCAS FERNANDEZ", 0, 0, 3);
+        newAdministrative(a4, 4, 130, "MARCOS JUAREZ", 0, 0, 4);
+        newAdministrative(a5, 5, 140, "PEDRO HUSARES", 0, 0, 5);
     }
 
     @AfterEach
     public void tearDown() {
         System.out.println("Limpiando variables...");
-        administrative = null;
+        a1 = null;
+        a2 = null;
+        a3 = null;
+        a4 = null;
+        a5 = null;
     }
 
     @Test
-    public void getRemunerativeTest() {
-        Double result = Amount.BASIC_SALARY + Amount.CATEGORY2 + (3 * Amount.SENIORITY);
-        assertEquals(result, administrative.getRemunerative());
+    public void getRemunerativeEqualsCategory1Test() {
+        System.out.println("Calculando remunerativos...");
+        Double result = Amount.BASIC_SALARY + Amount.CATEGORY1 + (a1.getSeniority() * Amount.SENIORITY);
+        assertEquals(result, a1.getRemunerative());
     }
 
     @Test
-    public void getPaymentChildrenTest() {
-        Double result = 2 * Amount.CHILD;
-        assertEquals(result, administrative.getPaymentChildren());
+    public void getRemunerativeEqualsCategory2Test() {
+        System.out.println("Calculando remunerativos...");
+        Double result = Amount.BASIC_SALARY + Amount.CATEGORY2 + (a2.getSeniority() * Amount.SENIORITY);
+        assertEquals(result, a2.getRemunerative());
     }
 
     @Test
-    public void getDiscountTest() {
-        Double result = administrative.getRemunerative() * Amount.DISCOUNT;
-        assertEquals(result, administrative.getDiscount());
+    public void getRemunerativeEqualsCategory3Test() {
+        System.out.println("Calculando remunerativos...");
+        Double result = Amount.BASIC_SALARY + Amount.CATEGORY3 + (a3.getSeniority() * Amount.SENIORITY);
+        assertEquals(result, a3.getRemunerative());
+    }
+
+    @Test
+    public void getRemunerativeEqualsCategory4Test() {
+        System.out.println("Calculando remunerativos...");
+        Double result = Amount.BASIC_SALARY + Amount.CATEGORY4 + (a4.getSeniority() * Amount.SENIORITY);
+        assertEquals(result, a4.getRemunerative());
+    }
+
+    @Test
+    public void getRemunerativeEqualsCategory5Test() {
+        System.out.println("Calculando remunerativos...");
+        Double result = Amount.BASIC_SALARY + Amount.CATEGORY5 + (a5.getSeniority() * Amount.SENIORITY);
+        assertEquals(result, a5.getRemunerative());
     }
 
     @Test
     public void getSalaryTest() {
-        Double result = administrative.getRemunerative() + administrative.getPaymentChildren() - administrative.getDiscount();
-        assertEquals(result, administrative.getSalary());
+        System.out.println("Calculando salario neto...");
+        Double r1 = a1.getRemunerative() + a1.getPaymentChildren() - a1.getDiscount();
+        assertEquals(r1, a1.getSalary());
+
+        Double r2 = a2.getRemunerative() + a2.getPaymentChildren() - a2.getDiscount();
+        assertEquals(r2, a2.getSalary());
+
+        Double r3 = a3.getRemunerative() + a3.getPaymentChildren() - a3.getDiscount();
+        assertEquals(r3, a3.getSalary());
+
+        Double r4 = a4.getRemunerative() + a4.getPaymentChildren() - a4.getDiscount();
+        assertEquals(r4, a4.getSalary());
+
+        Double r5 = a5.getRemunerative() + a5.getPaymentChildren() - a5.getDiscount();
+        assertEquals(r5, a5.getSalary());
+    }
+
+    private void newAdministrative(Administrative adm, Integer id, Integer record, String name, Integer children, Integer seniority, Integer category) {
+        adm.setId(id);
+        adm.setRecord(record);
+        adm.setName(name);
+        adm.setChildren(children);
+        adm.setSeniority(seniority);
+        adm.setCategory(category);
     }
 }
--- tp2/gestionsueldos/src/test/java/ar/edu/unju/fi/gestionsueldos/ProfessionalTest.java
+++ tp2/gestionsueldos/src/test/java/ar/edu/unju/fi/gestionsueldos/ProfessionalTest.java
@@ -5,51 +5,75 @@ import ar.edu.unju.fi.gestionsueldos.util.Amount;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 public class ProfessionalTest {
-    private Professional professional;
+    private Professional p1, p2;
 
     @BeforeEach
     public void setUp() {
         System.out.println("Configurando SetUp...");
-        professional = new Professional();
-        professional.setId(1);
-        professional.setRecord(100);
-        professional.setName("JUAN PEREZ");
-        professional.setChildren(1);
-        professional.setSeniority(10);
+        p1 = new Professional();
+        p2 = new Professional();
+        newProfessional(p1, 1, 100, "JUAN PEREZ", 2, 10);
+        newProfessional(p2, 2, 110, "LUIS DIAZ", 0, 0);
     }
 
     @AfterEach
     public void tearDown() {
         System.out.println("Limpiando variables...");
-        professional = null;
+        p1 = null;
+        p2 = null;
     }
 
     @Test
     public void getRemunerativeTest() {
-        Double result = Amount.BASIC_SALARY + Amount.DEGREE + (10 * Amount.SENIORITY);
-        assertEquals(result, professional.getRemunerative());
+        System.out.println("Calculando remunerativos...");
+        Double result1 = Amount.BASIC_SALARY + Amount.DEGREE + (p1.getSeniority() * Amount.SENIORITY);
+        assertEquals(result1, p1.getRemunerative());
+        Double result2 = Amount.BASIC_SALARY + Amount.DEGREE + (p2.getSeniority() * Amount.SENIORITY);
+        assertEquals(result2, p2.getRemunerative());
     }
 
     @Test
     public void getPaymentChildrenTest() {
-        Double result = 1 * Amount.CHILD;
-        assertEquals(result, professional.getPaymentChildren());
+        System.out.println("Calculando salario familiar...");
+        Double result = p1.getChildren() * Amount.CHILD;
+        assertEquals(result, p1.getPaymentChildren());
+    }
+
+    @Test
+    public void getPaymentWithoutChildrenTest() {
+        System.out.println("Calculando salario familiar...");
+        assertEquals(0, p2.getPaymentChildren());
     }
 
     @Test
     public void getDiscountTest() {
-        Double result = professional.getRemunerative() * Amount.DISCOUNT;
-        assertEquals(result, professional.getDiscount());
+        System.out.println("Calculando descuentos...");
+        Double result1 = p1.getRemunerative() * Amount.DISCOUNT;
+        assertEquals(result1, p1.getDiscount());
+        Double result2 = p2.getRemunerative() * Amount.DISCOUNT;
+        assertEquals(result2, p2.getDiscount());
     }
 
     @Test
     public void getSalaryTest() {
-        Double result = professional.getRemunerative() + professional.getPaymentChildren() - professional.getDiscount();
-        assertEquals(result, professional.getSalary());
+        System.out.println("Calculando salario neto...");
+        Double result1 = p1.getRemunerative() + p1.getPaymentChildren() - p1.getDiscount();
+        assertEquals(result1, p1.getSalary());
+        Double result2 = p2.getRemunerative() + p2.getPaymentChildren() - p2.getDiscount();
+        assertEquals(result2, p2.getSalary());
     }
 
+    private void newProfessional(Professional pro, Integer id, Integer record, String name, Integer children, Integer seniority) {
+        pro.setId(id);
+        pro.setRecord(record);
+        pro.setName(name);
+        pro.setChildren(children);
+        pro.setSeniority(seniority);
+    }
 }
